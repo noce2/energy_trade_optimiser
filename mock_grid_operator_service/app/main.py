4656d537d3d43d7702fde029b0a924abc18e9def
noce2 <20339399+noce2@users.noreply.github.com>
2021-11-08 09:33:09 +0000

    Add mock operator svc

--- mock_grid_operator_service/app/main.py
+++ mock_grid_operator_service/app/main.py
@@ -1,10 +1,44 @@
 from typing import Optional
-
+from random import choices
 from fastapi import FastAPI
 
 app = FastAPI()
+
+from pydantic import BaseModel
+from decimal import Decimal
+
+BID_OFFER_PAIR_ACCEPTANCE_RATE = 0.8
+
+
+class BidOfferPair(BaseModel):
+    submissionTime: str
+    settlementPeriodStartTime: str
+    offerPrice: Decimal
+    offerVolume: Decimal
+    bidPrice: Decimal
+    bidVolume: Decimal
+
+
+class BidOfferPairSubmissionResult(BidOfferPair):
+    accepted: bool
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "from mock grid operator"}
+
+
+@app.post("/submissions", response_model=BidOfferPairSubmissionResult)
+def evaluate_offer_or_bid(bidOfferPair: BidOfferPair) -> BidOfferPairSubmissionResult:
+    result = BidOfferPairSubmissionResult(
+        submissionTime=bidOfferPair.submissionTime,
+        settlementPeriodStartTime=bidOfferPair.settlementPeriodStartTime,
+        offerPrice=bidOfferPair.offerPrice,
+        offerVolume=bidOfferPair.offerVolume,
+        bidPrice=bidOfferPair.bidPrice,
+        bidVolume=bidOfferPair.bidVolume,
+        accepted=choices(
+            [True, False], cum_weights=(BID_OFFER_PAIR_ACCEPTANCE_RATE, 1.00), k=1
+        )[0],
+    )
+    return result
